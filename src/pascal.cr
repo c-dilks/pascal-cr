53644b1e5ea942c1d063837a7a5aca03282cc5f8
Christopher Dilks <c-dilks@users.noreply.github.com>
2022-07-16 03:05:08 -0400

    graphical output with celestine (#1)
    * graphical output with celestine
    
    * svg output works

--- src/pascal.cr
+++ src/pascal.cr
@@ -1,18 +1,25 @@
 require "big"
+require "celestine"
 
 module Pascal
 
   # store one row of the pascal triangle
   class Row
     getter nums, mods
+    DRAW_SIZE = 5
 
-    def initialize(@nums : Array(BigInt) = [BigInt.new 1])
-      @mods = @nums
+    def initialize(
+      @nums : Array(BigInt) = [BigInt.new 1], # initial row
+      @rowNumMax : Int64 = 0,                 # maximum row (for drawing; if zero, draw left-aligned)
+    )
+      @mods   = @nums
+      @rowNum = 0
     end
 
     # compute the next row of the triangle
     def next
       @nums = ([0]+@nums).zip(@nums+[0]).map &.sum
+      @rowNum += 1
     end
 
     # run a calculation on a row's `@nums`;
@@ -26,11 +33,25 @@ module Pascal
       self.modify do |num| num.modulo m end
     end
 
-    # output method sandbox
+    # text output
     def output(stream=STDOUT)
-      stream = STDOUT # force stdout
       stream.puts @nums
       # stream.puts @mods
+    end
+
+    # svg output
+    def draw(ctx)
+      @mods.each_with_index do |num,colNum|
+        ctx.rectangle do |rec|
+          offset     = ( @rowNumMax - @rowNum ) / 2
+          rec.x      = ( offset + colNum ) * DRAW_SIZE
+          rec.y      = @rowNum * DRAW_SIZE
+          rec.width  = DRAW_SIZE
+          rec.height = DRAW_SIZE
+          rec.fill   = "##{num}#{num}#{num}"
+          rec
+        end
+      end
     end
 
   end
