fa07f16ba3edceb63653533ed43804a05c258d85
christopher dilks <christopher.j.dilks@gmail.com>
2022-07-17 18:13:15 -0400

    new file:   src/colors.cr

--- src/pascal.cr
+++ src/pascal.cr
@@ -1,19 +1,22 @@
 require "big"
 require "celestine"
+require "./colors"
 
 module Pascal
 
   # store one row of the pascal triangle
   class Row
     getter nums, mods
-    property drawSize
+    property drawSize, palette, rowNumMax
 
     def initialize(
-      @nums : Array(BigInt) = [BigInt.new 1], # initial row
+      @rowNumMax : Int128,                    # maximum number of rows
+      @nums : Array(BigInt) = [BigInt.new 1]  # initial row
     )
       @mods     = @nums
       @rowNum   = 0
       @drawSize = 1.0
+      @palette  = Colors::Palette.new
     end
 
     # compute the next row of the triangle
@@ -39,34 +42,16 @@ module Pascal
       # stream.puts @mods
     end
 
-    # color interpolator
-    def colorize(num)
-      # color1 = [ 0,   0,   0   ]
-      # color2 = [ 255, 255, 255 ]
-      color1 = [ 10, 0,   0   ]
-      color2 = [ 0,  255, 255 ]
-      colorLerp = color1.zip(color2).map do |c1,c2|
-        c = c1+(c2-c1)*num
-        c.to_i
-      end
-      colorI = colorLerp[2] + (colorLerp[1]<<8) + (colorLerp[0]<<16)
-      colorHex = colorI.to_s(16)
-      (6-colorHex.size).times do colorHex="0"+colorHex end
-      colorHex = "#"+colorHex
-      # puts "#{num} to #{colorHex}"
-      colorHex
-    end
-
     # svg output
-    def draw(ctx,rowNumMax,numMin,numMax)
+    def draw(ctx)
       @mods.each_with_index do |num,colNum|
         ctx.rectangle do |rec|
-          offset     = ( rowNumMax - @rowNum ) / 2
+          offset     = ( @rowNumMax - @rowNum ) / 2
           rec.x      = ( offset + colNum ) * @drawSize
           rec.y      = @rowNum * @drawSize
           rec.width  = @drawSize
           rec.height = @drawSize
-          rec.fill   = self.colorize (num-numMin)/(numMax-numMin)
+          rec.fill   = @palette.colorize num
           rec
         end
       end
