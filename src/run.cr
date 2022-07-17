0cf9fb044303f21eac431de78dbd71f37c5c352e
christopher dilks <christopher.j.dilks@gmail.com>
2022-07-16 23:41:05 -0400

    modified:   src/run.cr

--- src/run.cr
+++ src/run.cr
@@ -8,12 +8,15 @@ SEED     = ( ARGV[2]? || 1   ).to_i # number in first row
 p! NUM_ROWS, MODULUS, SEED
 
 # settings
-OUTPUT_TXT = true
+OUTPUT_TXT = false
 OUTPUT_SVG = true
-CONCURRENT_BUFFER_SIZE = 5
+CONCURRENT_BUFFER_SIZE = 1
 
 # execution ---------------------------------------------------
-channel = Channel(Pascal::Row).new(CONCURRENT_BUFFER_SIZE)
+# channel = Channel(Pascal::Row).new(CONCURRENT_BUFFER_SIZE)
+channel = Array(Channel(Pascal::Row)).new(NUM_ROWS)
+done    = Array(Channel(Nil)).new(NUM_ROWS)
+mutex   = Mutex.new
 outFile = File.new("output.txt","w") if OUTPUT_TXT
 outSvg  = File.new("output.svg","w") if OUTPUT_SVG
 svg = Celestine.draw do |ctx|
@@ -23,33 +26,44 @@ svg = Celestine.draw do |ctx|
 
   # output proc
   output = ->(t : Pascal::Row) {
+    mutex.lock
     t.output outFile.as(File) if OUTPUT_TXT
     t.draw ctx, NUM_ROWS+1 if OUTPUT_SVG
+    mutex.unlock
   }
   output.call triangle # output seed row
 
   # `next` fibers: compute the next row, stored in `@nums` of `triangle`. Sends
   # a clone of `triangle` to the buffer
-  spawn do
-    NUM_ROWS.times do |i|
+  NUM_ROWS.times do |i|
+    channel << Channel(Pascal::Row).new(CONCURRENT_BUFFER_SIZE)
+    done << Channel(Nil).new
+    spawn same_thread: true do
       puts "next: #{i}"
       triangle.next
-      channel.send(triangle.clone)
+      channel[i].send triangle.clone
     end
   end
 
   # `modify` fibers: run the desired `modify` function on the `Row` received
   # from the buffer
-  spawn do
-    NUM_ROWS.times do |i|
-      t = channel.receive
+  NUM_ROWS.times do |i|
+    spawn do
+      t = channel[i].receive
       puts "#{" "*30}modify: #{i}"
       t.modulo MODULUS
       output.call t
+      done[i].send nil
     end
   end
 
   # yield to fibers
+  NUM_ROWS.times do |i|
+    spawn do
+      done[i].receive
+      puts "-> done #{i}"
+    end
+  end
   Fiber.yield
 end
 
